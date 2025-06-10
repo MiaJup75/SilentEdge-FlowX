883b1c643cf050690374c53898161cd19fdc5111
MiaJup75 <miajupiter75@gmail.com>
2025-06-10 14:12:04 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_USER_ID = int(os.getenv("TELEGRAM_USER_ID"))
+SOLFLARE_PRIVATE_KEY = os.getenv("SOLFLARE_PRIVATE_KEY")
+JUPITER_API_URL = "https://quote-api.jup.ag/v6"
