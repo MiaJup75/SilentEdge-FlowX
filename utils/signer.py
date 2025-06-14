38b170e4304735dd78d46c834613a356d623445a
MiaJup75 <miajupiter75@gmail.com>
2025-06-14 20:46:26 +0700

    Update signer.py

--- utils/signer.py
+++ utils/signer.py
@@ -5,9 +5,9 @@ from solana.keypair import Keypair
 import os
 
 def load_wallet_from_env():
-    secret_key_b64 = os.getenv("PHANTOM_SECRET_KEY")
+    secret_key_b64 = os.getenv("SOLANA_SECRET_KEY")
     if not secret_key_b64:
-        raise Exception("PHANTOM_SECRET_KEY is missing")
+        raise Exception("SOLANA_SECRET_KEY is missing")
 
     secret_key_bytes = base64.b64decode(secret_key_b64)
     keypair = Keypair.from_secret_key(secret_key_bytes)
