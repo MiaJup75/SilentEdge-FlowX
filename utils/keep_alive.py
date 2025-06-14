f4eddcb2a34ee90608352a7991322623c5a85aea
MiaJup75 <miajupiter75@gmail.com>
2025-06-14 10:24:06 +0700

    Create keep_alive.py

---
+++ utils/keep_alive.py
@@ -0,0 +1,11 @@
+# keep_alive.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Flow X Bot is alive!", 200
+
+def run():
+    app.run(host="0.0.0.0", port=8443)
