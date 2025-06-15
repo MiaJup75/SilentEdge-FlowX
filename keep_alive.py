50309a19ebfccf75a17304afe2104a87d8323ca7
MiaJup75 <miajupiter75@gmail.com>
2025-06-15 12:30:23 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,12 +1,12 @@
 # keep_alive.py
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "✅ Flow X Bot is alive!", 200
 
 def run():
-    # Use a safe non-conflicting port like 8080
     app.run(host="0.0.0.0", port=8080)
